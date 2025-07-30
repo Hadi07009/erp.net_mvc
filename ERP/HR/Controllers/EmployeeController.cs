ea88e878b201fd9b25a955b3021c4ddf9554b778
Hadi07009 <hadi.mbstu@gmail.com>
2025-07-30 16:56:41 +0600

    Save Employee Information

---
+++ ERP/HR/Controllers/EmployeeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HR.Models;
+
+namespace HR.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewEmployee(Employee objEmployee)
+        {
+            string employeeName = objEmployee.Name;
+            string employeeEmail = objEmployee.Email;
+            string connectionString = ConfigurationManager.ConnectionStrings["dbERPConnection"].ToString();
+            var sql = @"INSERT INTO [Employee]
+               ([Name], [EmployeeEmail]
+        
+		       )
+                VALUES (" +
+               "'" + employeeName + "'" +
+                ",'" + employeeEmail + "'" +
+               " );";
+            
+            SaveEmployee(connectionString, sql);
+            return View();
+        }
+
+        private static void SaveEmployee(string connectionString, string sql)
+        {
+            try
+            {
+                var myConnection = new SqlConnection(connectionString);
+                myConnection.Open();
+                new SqlCommand(sql, myConnection).ExecuteNonQuery();
+                myConnection.Close();
+            }
+            catch (Exception msgError)
+            {
+
+                throw msgError;
+            }
+        }
+
+        public ActionResult ShowEmployee()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ERP/HR/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HR.Models
+{
+    public class Employee
+    {
+        string _name;
+        string _email;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+    }
+}
