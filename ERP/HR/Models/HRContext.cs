1b21d4f531529a6733c17c9ec3680b5aa0cf452d
Hadi07009 <hadi.mbstu@gmail.com>
2025-08-12 11:56:36 +0600

    Class 10- Entity Framework Code First Approch

---
+++ ERP/HR/Models/HRContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HR.Models
+{
+    public class HRContext :DbContext
+    {
+        public HRContext() : base("dbERPConnection")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
